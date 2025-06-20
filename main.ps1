98d4179a013709cb76f98ab68603fc3eaa25efc0
turnsbacktime <turnsbacktime@outlook.com>
2025-06-20 21:15:16 +0000

    commit

--- main.ps1
+++ main.ps1
@@ -1,1 +1,6 @@
-& "C:\Users\Admin\Documents\GitHub\casino-bot\venv\Scripts\python.exe" "C:\Users\Admin\Documents\GitHub\casino-bot\main.py"
+# Activate python virtual environment and run the main script
+& "C:\Users\Admin\Documents\GitHub\casino-bot\venv\Scripts\Activate.ps1"
+# Install required packages
+pip install -r "C:\Users\Admin\Documents\GitHub\casino-bot\requirements.txt"
+# Run the main Python script
+python "C:\Users\Admin\Documents\GitHub\casino-bot\main.py"
