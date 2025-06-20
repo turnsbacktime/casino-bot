00d4fb9773fb39f7e9d8d5a388f8d58fd3c0ba4f
turnsbacktime <turnsbacktime@outlook.com>
2025-06-20 22:53:13 +0700

    commit

--- main.ps1
+++ main.ps1
@@ -1,15 +1,1 @@
-# Connect to remote host via SSH and run a terminal script
-$hostname = "pigletaccess"
-$username = "admin"
-$scriptToRun = "echo 'Pigletaccess'"
-# Run the script remotely using ssh.exe
-$sshCommand = "ssh $username@$hostname $scriptToRun"
-Invoke-Expression $sshCommand
-# Activate Python virtual environment and run main.py using Python
-$venvActivate = "C:\Users\Admin\Documents\GitHub\casino-bot\venv\Scripts\Activate.ps1"
-# Activate the virtual environment
-& $venvActivate
-# Run python script
-$pythonScript = "C:\Users\Admin\Documents\GitHub\casino-bot\main.py"
-# Run the Python script
-python $pythonScript
+& "C:\Users\Admin\Documents\GitHub\casino-bot\venv\Scripts\python.exe" "C:\Users\Admin\Documents\GitHub\casino-bot\main.py"
